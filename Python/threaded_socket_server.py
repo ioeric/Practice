7101267584af5cafcae9ae981a4103331946d74c
Eric Liu <Eric@res2net-129-97-232-212.dynamic.uwaterloo.ca>
2013-04-15 18:59:25 -0400

    Add INT practice

--- Python/threaded_socket_server.py
+++ Python/threaded_socket_server.py
@@ -6,7 +6,7 @@ class ThreadedRequestHandler(SocketServer.BaseRequestHandler):
         data = self.request.recv(1024)
         cur_thread = threading.currentThread()
         print 'Server received "%s":"%s"' %(cur_thread.getName(),data)
-        reponses = '%s: %s' %(cur_thread.getName(), data)
+        responses = '%s: %s' %(cur_thread.getName(), data)
         self.request.send(responses)
         return
 class ThreadedSocketServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
@@ -35,5 +35,18 @@ if __name__ == '__main__':
     print 'Received: "%s"' %response 
     print 'Closing socket' 
     s.close()
+    
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s1.connect((ip, port))
+
+    message1 = 'Hello, Waterloo!'
+    print 'Sending message "%s"' %message
+    len_sent1 = s1.send(message)
+    print 'Sent "%s", length:%d' %(message1, len_sent1)
+    response1 = s1.recv(1024)
+    print 'Received: "%s"' %response1 
+    print 'Closing socket' 
+    s1.close()
+
     print 'Closing server socket'
     server.socket.close()
